5a78fc9e85220aba6b86ba0c576224b683548b63
pamosima <pamosima@cisco.com>
2023-03-27 18:22:11 +0200

    added pnp claim to site

--- myscript.py
+++ myscript.py
@@ -20,11 +20,18 @@ import json
 # The base_url used by default is `from dnacentersdk.config import DEFAULT_BASE_URL`
 api = DNACenterAPI(username='admin', password='C1sco12345', base_url="https://dnac.its-best.ch:443", version='2.3.3.0', verify=False)
 
-siteId=api.sites.get_site(name="Global/Denner/Aargau/Store-101/Store-101-1")["response"][0]["id"]
+site ="Global/Denner/Aargau/Store-101/Store-101-1"
+templateName = "C9k_Onboarding-Template"
+p2p_onboarding_ip_address = "172.20.10.2"
+p2p_onboarding_gw = "172.20.10.1"
+p2p_onbarding_vlan = "20"
+hostname = "store010-s20006"
+
+siteId=api.sites.get_site(name=site)["response"][0]["id"]
 deviceId=api.device_onboarding_pnp.get_device_list(serial_number="JAE231609EK")[0]["id"]
-templateId=api.configuration_templates.get_templates_details(name="C9k_Onboarding-Template")["response"][0]["id"]
+templateId=api.configuration_templates.get_templates_details(name=templateName)["response"][0]["id"]
 print("siteId="+siteId)
 print("deviceId="+deviceId)
 print("templateId="+templateId)
 
-api.device_onboarding_pnp.claim_a_device_to_a_site(configInfo=None, deviceId=deviceId, siteId=siteId)
+api.device_onboarding_pnp.claim_a_device_to_a_site(configInfo={'configId': templateId, 'configParameters': [{'key': 'HOSTNAME', 'value': hostname},{'key': 'P2P_ONBOARDING_IP_ADDRESS', 'value': p2p_onboarding_ip_address},{'key': 'P2P_ONBOARDING_GW', 'value': p2p_onboarding_gw},{'key': 'P2P_ONBOARDING_VLAN', 'value': p2p_onbarding_vlan}]}, hostname=hostname,  deviceId=deviceId, siteId=siteId, type="Default")
